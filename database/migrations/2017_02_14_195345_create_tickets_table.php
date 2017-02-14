e6b26950e7d35aa8f50bdb889ebd8041885a3e3e
Victor Palomo <ing.victorpalomo@gmail.com>
2017-02-14 14:09:41 -0600

    Primeras migraciones

--- database/migrations/2017_02_14_195345_create_tickets_table.php
+++ database/migrations/2017_02_14_195345_create_tickets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateTicketsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,13 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('tickets', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('last_name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
+			$table->string('title', 200);
+			$table->enum('status',['open','closed']);
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
 			$table->timestamps();
 		});
 	}
@@ -31,7 +30,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('tickets');
 	}
 
 }
